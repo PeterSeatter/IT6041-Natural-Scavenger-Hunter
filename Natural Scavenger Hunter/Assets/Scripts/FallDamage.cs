2e9ca0ba41c1d0120f4cb351d56a457328f884bd
PeterSeatter <56239159+PeterSeatter@users.noreply.github.com>
2020-10-30 20:21:23 +1300

    Health working but constant Damage
    Got a health syystem working but when fall damage is given it'll constantly remove health from the player.

---
+++ Natural Scavenger Hunter/Assets/Scripts/FallDamage.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour
+{
+    public float LastPositionY = 0f;
+    public float FallDistance = 0f;
+    public Transform Player;
+    private CharacterController Controller;
+    private HealthManager HM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+        //HealthManager HM = gameObject.GetComponent("HealthManager") as HealthManager;
+        HealthManager HM = gameObject.GetComponent<HealthManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FallDamageAmount();        
+    }
+
+    public void FallDamageAmount()
+    {    
+        if (LastPositionY > Player.transform.position.y)
+        {
+            FallDistance += LastPositionY - Player.transform.position.y;
+        }
+
+        LastPositionY = Player.transform.position.y;
+
+        if (FallDistance >= 5 && Controller.isGrounded)
+        {
+            HM.CurrentHealth -= Mathf.RoundToInt(FallDistance);
+            FallReset();
+            Debug.Log("You've hit the ground");
+        }
+
+        if (FallDistance <= 5 && Controller.isGrounded)
+        {
+            FallReset();
+        }
+    }
+
+    private void FallReset()
+    {
+        FallDistance = 0f;
+        LastPositionY = 0f;
+        //Debug.Log("It's reset for another drop");
+    }
+}
---
+++ Natural Scavenger Hunter/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    private static HealthManager _instance;
+    public static HealthManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public int MaxHealth;
+    public int CurrentHealth;
+    public float LastPositionY = 0f;
+    public float FallDistance = 0f;
+    public Transform Player;
+    private CharacterController Controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+        Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FallDamageAmount();
+    }
+
+    private void FallDamageAmount()
+    {
+        if (LastPositionY > Player.transform.position.y)
+        {
+            FallDistance += LastPositionY - Player.transform.position.y;
+        }
+
+        LastPositionY = Player.transform.position.y;
+
+        if (FallDistance >= 5 && Controller.isGrounded)
+        {
+            CurrentHealth -= Mathf.RoundToInt(FallDistance);
+            FallReset();
+            //Debug.Log("You've hit the ground");
+        }
+
+        if (FallDistance <= 5 && Controller.isGrounded)
+        {
+            FallReset();
+        }
+    }
+
+    private void FallReset()
+    {
+        FallDistance = 0f;
+        LastPositionY = 0f;
+        //Debug.Log("It's reset for another drop");
+    }
+}
