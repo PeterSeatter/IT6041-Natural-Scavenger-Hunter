277d845a72022cf549befb8715fedc922aecd46b
PeterSeatter <56239159+PeterSeatter@users.noreply.github.com>
2020-11-11 15:44:41 +1300

    interaction completed
    Completed the interaction with only the need to put a pop up for when an interaction is available.

---
+++ Natural Scavenger Hunter/Assets/Scripts/Raycast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    private GameObject RaycastedObject;
+
+    [SerializeField] private int RayLength = 10;
+    [SerializeField] private LayerMask LayerMaskInteract;
+
+    private void Update()
+    {
+        RaycastHit Hit;
+        Vector3 Forward = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, Forward, out Hit, RayLength, LayerMaskInteract.value))
+        {
+            if (Hit.collider.CompareTag("Object"))
+            {
+                RaycastedObject = Hit.collider.gameObject;
+
+                if (Input.GetKeyDown("f"))
+                {
+                    Debug.Log("You've interacted with the object");
+                    //removes the object from the world
+                    //RaycastedObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
