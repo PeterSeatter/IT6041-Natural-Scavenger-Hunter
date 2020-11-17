89ec258134d65d0ff1b978433909c5cdb45dc3eb
PeterSeatter <56239159+PeterSeatter@users.noreply.github.com>
2020-11-18 11:30:19 +1300

    Interaction pop up
    Got the pop up working with the interaction

--- Natural Scavenger Hunter/Assets/Scripts/Raycast.cs
+++ Natural Scavenger Hunter/Assets/Scripts/Raycast.cs
@@ -8,32 +8,57 @@ public class Raycast : MonoBehaviour
     [SerializeField] private LayerMask LayerMaskInteract;
     public int Wood;
     public int Bamboo;
+    public Transform InteractionPrompt;
 
 
     private void Update()
+    {
+        Raycaster();
+    }
+
+    public void Raycaster()
     {
         RaycastHit Hit;
         Vector3 Forward = transform.TransformDirection(Vector3.forward);
 
         if (Physics.Raycast(transform.position, Forward, out Hit, RayLength, LayerMaskInteract.value))
         {
+            if (RayLength >= 10 && Hit.collider.CompareTag("Wood"))
             {
-                if (Input.GetKeyDown("f"))
+                if (InteractionPrompt.gameObject.activeInHierarchy == false)
                 {
-                    Debug.Log("You've interacted with the object");
-                    //removes the object from the world
-                    //RaycastedObject.SetActive(false);
-
-                    if (Hit.collider.CompareTag("Wood"))
-                    {
-                        FindObjectOfType<Collected>().AddWood(Wood);
-                    }
-
-                    if (Hit.collider.CompareTag("Bamboo"))
-                    {
-                        FindObjectOfType<Collected>().AddBamboo(Bamboo);
-                    }
+                    InteractionPrompt.gameObject.SetActive(true);
                 }
+            }
+
+            if (RayLength >= 10 && Hit.collider.CompareTag("Bamboo"))
+            {
+                if (InteractionPrompt.gameObject.activeInHierarchy == false)
+                {
+                    InteractionPrompt.gameObject.SetActive(true);
+                }
+            }
+        }
+        else
+        {
+            //turn off panel
+            InteractionPrompt.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown("f"))
+        {
+            Debug.Log("You've interacted with the object");
+            //removes the object from the world
+            //RaycastedObject.SetActive(false);
+
+            if (Hit.collider.CompareTag("Wood"))
+            {
+                FindObjectOfType<Collected>().AddWood(Wood);
+            }
+
+            if (Hit.collider.CompareTag("Bamboo"))
+            {
+                FindObjectOfType<Collected>().AddBamboo(Bamboo);
             }
         }
     }
